850b34ce70450f3d058ada2bc799f533c490ab63
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-07-24 15:28:54 +0200

    TP scripting avec Node - partie données

--- presentation.js
+++ presentation.js
@@ -1,29 +1,37 @@
 const readline = require('readline');
+const service = require('./service.js')
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 
-  function menu() {
+function menu() {
     console.log('1. Lister les clients');
     console.log('99. Sortir');
-  }
+}
 
-function start () {
-    
+function start() {
+
     menu();
 
     rl.question('', function (saisie) {
 
-        switch(saisie){
-            case '1' : console.log(">>Liste des clients");
-            menu();
+        switch (saisie) {
+            case '1':
+                console.log(">>Liste des clients");
+                // affichage du body qui contient les clients
+                service.getClients((client) => {
+                    console.log(client);
+                    // reaffichage du menu 
+                    start();
+                });
                 break;
-            case '99' : console.log("Adios Amigos");
+            case '99':
+                console.log("Adios Amigos");
+                rl.close();
                 break;
         }
-        rl.close();
     });
 
 }
